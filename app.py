4e7ad360df450d8e59a12a22a022a74cf68c3871
kimchi-n-cheese <132268113+kimchi-n-cheese@users.noreply.github.com>
2023-05-01 16:04:08 +0000

    added dynamic data and api route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+TOUR_PACKAGE = [
+  {
+    'id': 1,
+    'title': 'Vietnam',
+    'duration': '7 days',
+    'price': 'Starting at $600'
+  },
+  {
+    'id': 2,
+    'title': 'Laos',
+    'duration': '4 days',
+    'price': 'Starting at $350'
+  },
+  {
+    'id': 3,
+    'title': 'Cambodia',
+    'duration': '4 days',
+    'price': 'Starting at $360'
+  },
+  {
+    'id': 4,
+    'title': 'Thailand',
+    'duration': '5 days',
+    'price': 'Starting at $600'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template('home.html', deals=TOUR_PACKAGE)
+
+@app.route("/api/tours")
+def list_tours():
+  return jsonify(TOUR_PACKAGE)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
